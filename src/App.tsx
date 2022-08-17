b557341f605cf2f73d3a2915e7cbf0fd6d4bfa98
Thayron Graco <thayron.21@gmail.com>
2022-08-17 20:54:29 -0300

    adding eventlistener keydown

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, useState, KeyboardEvent, useEffect } from "react";
 
 function App() {
+  const [todo, setTodo] = useState("");
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTodo(e.target.value);
+  };
+
+  useEffect(() => {
+    const listener = (event: any) => {
+      if (event.code === "Enter" || event.code === "NumpadEnter") {
+        console.log("Enter key was pressed. Run your function.");
+        event.preventDefault();
+        if (todo === "") {
+          alert("Preencha os campos!");
+        } else {
+          alert("Teste");
+        }
+      }
+    };
+    document.addEventListener("keydown", listener);
+    return () => {
+      document.removeEventListener("keydown", listener);
+    };
+  }, [todo]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main>
+        <h1>ToDo ReactJS</h1>
+        <div className="cardInputArea">
+          <input
+            id="todoInput"
+            type="text"
+            value={todo}
+            onChange={handleInputChange}
+            placeholder="Digite uma tarefa para ser adicionada..."
+          />
+        </div>
+      </main>
+    </>
   );
 }
 
